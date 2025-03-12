05f091897c7a5336e154c45fd9a2665958e7b7d1
vicrod8a <vico.emanuel.8a@gmail.com>
2025-03-12 13:13:04 -0600

    Update firebaseConfig.js
    cambio bd

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDGR-Gx5TKnMBoBCFP3-A710-uH27qVeig",
-  authDomain: "miapp-integral-cce35.firebaseapp.com",
-  projectId: "miapp-integral-cce35",
-  storageBucket: "miapp-integral-cce35.firebasestorage.app",
-  messagingSenderId: "647655000403",
-  appId: "1:647655000403:web:817cb309c0b471133279a6",
-  measurementId: "G-EY1D11FFRV"
+  apiKey: "AIzaSyBIpwSjCKA9D_khn4n5oqrNLMkPQ4asnTo",
+  authDomain: "miapp-integral-167c2.firebaseapp.com",
+  projectId: "miapp-integral-167c2",
+  storageBucket: "miapp-integral-167c2.firebasestorage.app",
+  messagingSenderId: "826153900809",
+  appId: "1:826153900809:web:5eebd8901fafd53c7911d8",
+  measurementId: "G-D9PXHGS201"
 };
 
 // Inicializa Firebase
@@ -21,3 +21,5 @@ const db = getFirestore(app);
 
 // Exporta la instancia de Firestore para que pueda ser usada en otros componentes
 export { db };
+
+
